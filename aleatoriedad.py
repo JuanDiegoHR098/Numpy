265107f9b028fdb8de5577c7abc19d5e44b7db6a
JuanDiegoHR098 <juan.hernandez48@udea.edu.co>
2024-04-02 22:06:21 -0500

    numpy, pandas, matplotlib

---
+++ aleatoriedad.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+#GENERA NUMEROS ENTEROS ALEATORIOS
+x= np.random.randint(0,10)
+print(x)
+
+#GENERA UN NUMERO DECIMAL ENTR EL 0 Y EL 1
+numero = np.random.rand()
+print(numero)
---
+++ rew.py
@@ -0,0 +1,12 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.plot([1,2,3,4],[1,4,6,8])
+
+plt.ylabel("y numbers")
+plt.xlabel("x numbers")
+
+
+plt.show()
+
